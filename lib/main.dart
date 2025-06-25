19e1d9e61f19734895163f557a9d93c8c667569b
Raphael Damascena <rapphael22@hotmail.com>
2025-06-24 21:10:10 -0300

    configuracao do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: unused_import
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:sistema_login_cadastro/login.dart';
 import 'package:sistema_login_cadastro/interna.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Login()));
 }
